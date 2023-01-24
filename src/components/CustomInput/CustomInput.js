14591bdd1066fef80ad6f403b05545f68e679bba
Christian-000 <christianpiccinini4@gmail.com>
2023-01-24 15:58:10 -0300

    [feature] CustomInput

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
-const CustomInput = () => {
+const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
   return (
     <View>
-      <Text>CustomInput</Text>
+      <TextInput
+        placeholder={placeholder}
+        style={styles.input}
+        value={value}
+        onChangeText={setValue}
+        secureTextEntry={secureTextEntry}
+        />
     </View>
   )
 }
-
+//SEGUIR VIENDO https://www.youtube.com/watch?v=ALnJLbjI7EY DESDE 14:49
 export default CustomInput
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: 'white',
+    width: '100%',
+
+    borderColor: '#e8e8e8',
+    borderWidth: 1,
+    borderRadius: 5,
+
+    paddingHorizontal: 10,
+    marginVertical: 5,
+  },
+  input: {},
+})
--- src/screens/SingIn/SingIn.js
+++ src/screens/SingIn/SingIn.js
@@ -1,13 +1,17 @@
 import { Image, StyleSheet, Text, View, useWindowDimensions } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import PlaceHolderLogo from '../../../assets/images/testLogo.jpg';
+import CustomInput from '../../components/CustomInput';
 
 const SingIn = () => {
     const { height } = useWindowDimensions();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
   return (
     <View style={styles.root}>
         <Image source={PlaceHolderLogo} style={[styles.logo, {height: height * 0.3}]} resizeMode="contain" />
-      <Text>SingIn</Text>
+        <CustomInput placeholder={'Email'} value={email} setValue={setEmail} />
+        <CustomInput placeholder={'Contraseña'} value={password} setValue={setPassword} />
     </View>
   )
 }
