a186ee1463ae71442f81a27b68a85093dd49be7b
solch4 <sol.maldonado.0209@gmail.com>
2023-10-23 03:53:13 -0300

    [HotFix] ConfirmEmail deleted

--- src/navigation/Stacks/JoinAppStack.js
+++ src/navigation/Stacks/JoinAppStack.js
@@ -3,7 +3,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Screens
 import SignIn from "../../screens/SignIn";
-import ConfirmEmail from "../../screens/ConfirmEmail";
 import ForgotPassword from "../../screens/ForgotPassword";
 import NewPassword from "../../screens/NewPassword";
 import Signature from "../../screens/Signature";
@@ -15,7 +14,6 @@ const JoinAppStackScreens = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="ConfirmEmail" component={ConfirmEmail} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="NewPassword" component={NewPassword} />
       <Stack.Screen name="HomeTab" component={HomeTabs} />
