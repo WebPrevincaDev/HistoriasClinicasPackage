ab3660960f6afdfdaf0e0e8ec5697bc0da9b219d
solch4 <sol.maldonado.0209@gmail.com>
2023-11-11 00:29:30 -0300

    [Feature] Home HCD screen base added

--- src/navigation/Stacks/HcdStack.js
+++ src/navigation/Stacks/HcdStack.js
@@ -2,16 +2,25 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Screens
 import HomeHCD from "../../screens/HcdScreens/HomeHCD";
+import MotivoDelLlamado from "../../screens/HcdScreens/MotivoDelLlamado";
 
 const HCDStack = createNativeStackNavigator();
 
 const HcdStack = () => {
   return (
-    <HCDStack.Navigator screenOptions={{ headerShown: false }}>
-      <HCDStack.Screen name="HomeHCD" component={HomeHCD} />
-      {/* ACA VAN A IR EL RESTO DE SCREENS RELACIONADAS CON LA CREACION DE UNA HCD */}
+    <HCDStack.Navigator>
+      <HCDStack.Screen
+        name="HomeHCD"
+        component={HomeHCD}
+        options={{ headerShown: false }}
+      />
+      <HCDStack.Screen
+        name="MotivoDelLlamado"
+        component={MotivoDelLlamado}
+        options={{ title: "Motivo del llamado" }}
+      />
     </HCDStack.Navigator>
   );
-}
+};
 
 export default HcdStack;
---
+++ src/screens/HcdScreens/MotivoDelLlamado.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MotivoDelLlamado() {
+  return (
+    <View>
+      <Text>MotivoDelLlamado</Text>
+    </View>
+  );
+}
