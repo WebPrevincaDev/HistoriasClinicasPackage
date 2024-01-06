cc5d7fb42f7b0fe525b7d9a57d250f781b1d6b5c
solch4 <sol.maldonado.0209@gmail.com>
2024-01-05 21:06:49 -0300

    [HotFix] HomeTabs icons updated

--- src/navigation/Tabs/HomeTabs.js
+++ src/navigation/Tabs/HomeTabs.js
@@ -2,7 +2,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // Icons
-import { MaterialCommunityIcons, Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 // Screens
 import Home from "../../screens/Home";
@@ -18,27 +18,41 @@ const HomeTabs = () => {
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            iconName = focused ? "home-circle-outline" : "home-circle";
-            return <MaterialCommunityIcons name="home-circle" size={size} color={color} />;
-          },
+          tabBarIcon: ({ focused, color, size }) => (
+            <MaterialCommunityIcons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
+          ),
         }}
       />
-      <Tab.Screen name="CrearHCD" component={HomeHCD} options={{
-        tabBarIcon: ({ focused, color, size}) => {
-          let iconName;
-          iconName = focused ? "add-circle-outline" : "add-circle";
-          return <Ionicons name={iconName} size={size} color={color} />;
-        }
-      }}/>
-      <Tab.Screen name="Perfil" component={ProfileStack} options={{
-        tabBarIcon: ({ focused, color, size}) => {
-          let iconColor;
-          iconColor = focused ? "blue" : "black";
-          return <FontAwesome5 name="user-md" size={size} color={iconColor} />;
-        }
-      }}/>
+      <Tab.Screen
+        name="CrearHCD"
+        component={HomeHCD}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <MaterialIcons
+              name={focused ? "add-circle" : "add-circle-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={ProfileStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <MaterialIcons
+              name={focused ? "person" : "person-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
