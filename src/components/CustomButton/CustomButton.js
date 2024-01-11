1c51f61272077ad9d842fdd976d07a7225f84310
solch4 <sol.maldonado.0209@gmail.com>
2024-01-11 20:03:28 -0300

    [HotFix] CustomButton code updated

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,67 +1,54 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, Pressable } from "react-native";
 
 const CustomButton = ({ onPress, text, disabled, type = "PRIMARY" }) => {
   return (
-    <Pressable disabled={disabled} onPress={onPress} style={[styles.container, styles[`container_${type}`], disabled && styles.containerDisabled]}>
+    <Pressable
+      disabled={disabled}
+      onPress={onPress}
+      style={[styles.container(disabled), styles[`container_${type}`]]}
+    >
       <Text style={[styles.text, styles[`text_${type}`]]}>{text}</Text>
     </Pressable>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-
+  container: (disabled) => ({
+    width: "100%",
     padding: 16,
     marginVertical: 5,
-
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 6,
-  },
-
-  containerDisabled: {
-    opacity: 0.6
-  },
-
+    opacity: disabled ? 0.6 : 1,
+  }),
   container_PRIMARY: {
-    backgroundColor: '#3B71F3',
+    backgroundColor: "#3B71F3",
   },
-
   container_SECONDARY: {
-    borderColor: '#3B71F3',
+    borderColor: "#3B71F3",
     borderWidth: 2,
   },
-
-  container_TERTIARY: {
-
-  },
-
+  container_TERTIARY: {},
   container_SIMPLE: {
-    padding: 16,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
 
   text: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-
   text_PRIMARY: {
-    color: 'white',
+    color: "white",
   },
-
   text_SECONDARY: {
-    color: '#3B71F3',
+    color: "#3B71F3",
   },
-
   text_TERTIARY: {
-    color: 'gray',
+    color: "gray",
   },
-
   text_SIMPLE: {
-    color: '#004dff',
+    color: "#004dff",
     fontSize: 20,
   },
-})
+});
