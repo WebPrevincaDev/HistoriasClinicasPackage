71cb4ccfa006d4859c57f9cf7319211594d93104
solch4 <sol.maldonado.0209@gmail.com>
2024-01-23 21:00:22 -0300

    [Feature] Previsualizacion screen finished

--- src/screens/HcdScreens/Previsualizacion.js
+++ src/screens/HcdScreens/Previsualizacion.js
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { WebView } from "react-native-webview";
+import CreatePdfService from "../../services/CreatePdfService";
+
+const cPdf = new CreatePdfService();
 
 export default function Previsualizacion() {
+  const { user } = useSelector((state) => state.auth);
+  const { hcd, hcdConfig } = useSelector((state) => state.hcd);
+
   return (
-    <View>
-      <Text>Previsualizacion</Text>
-    </View>
+    <WebView
+      source={{ html: cPdf.create_pdf({ hcd, hcdConfig, user }) }}
+      allowFileAccess={true}
+    />
   );
 }
