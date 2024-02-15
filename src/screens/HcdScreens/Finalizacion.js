b6415a4ba21bfc183bcd9ae249cf62510c436c95
solch4 <sol.maldonado.0209@gmail.com>
2024-02-15 00:51:30 -0300

    [HotFix] useFinishHcd hook created

--- src/screens/HcdScreens/Finalizacion.js
+++ src/screens/HcdScreens/Finalizacion.js
@@ -3,13 +3,10 @@ import { Text, Alert, Image } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import "react-native-get-random-values";
-import { v5 as uuidv5, v4 as uuidv4 } from "uuid";
-import * as Application from "expo-application";
 import * as Print from "expo-print";
 import { useDropdown } from "../../hooks/useDropdown";
-import { setHcdScreen, updateHcd } from "../../store/slices/hcd";
-import { addHcd, agregarPaciente } from "../../store/slices/hcd/thunks";
+import { useFinishHcd } from "../../hooks/useFinishHcd";
+import { updateHcd } from "../../store/slices/hcd";
 import { saveSignature } from "../../helpers/data";
 import { invalidInput } from "../../constants";
 import Container from "../../components/Container";
@@ -27,10 +24,10 @@ export default function Finalizacion() {
   const navigation = useNavigation();
   const { control, handleSubmit } = useForm();
   const { user } = useSelector((state) => state.auth);
-  const { hcd, hcdConfig, arr_hcd } = useSelector((state) => state.hcd);
+  const { hcd, hcdConfig } = useSelector((state) => state.hcd);
   const [signature, setSignature] = useState({});
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const { finishHcd, isLoading } = useFinishHcd();
 
   const {
     value: abonaCopagoValue,
@@ -64,21 +61,7 @@ export default function Finalizacion() {
   const onPressFinalizar = async (inputData) => {
     const datos = guardarDatos(inputData);
     if (!datos) return;
-    setIsLoading(true);
-    const finalHcd = {
-      ...hcd,
-      ...datos,
-      key: uuidv5("", uuidv5.DNS),
-      id_interno: uuidv4(),
-      version_app: Application.nativeBuildVersion,
-    };
-    dispatch(agregarPaciente(finalHcd));
-    await dispatch(
-      addHcd({ new_arr_hcd: [...arr_hcd, finalHcd], hcdConfig, user })
-    ).unwrap();
-    setIsLoading(false);
-    dispatch(setHcdScreen(""));
-    navigation.navigate("HomeTab");
+    await finishHcd(datos);
   };
 
   const onPressPrevisualizar = (inputData) => {
--- src/screens/HcdScreens/MotivoCancelacion.js
+++ src/screens/HcdScreens/MotivoCancelacion.js
@@ -1,40 +1,14 @@
-import "react-native-get-random-values";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import * as Application from "expo-application";
-import { v5 as uuidv5, v4 as uuidv4 } from "uuid";
-import { setHcdScreen } from "../../store/slices/hcd";
-import { addHcd } from "../../store/slices/hcd/thunks";
+import { useFinishHcd } from "../../hooks/useFinishHcd";
 import Container from "../../components/Container";
 import CustomInput from "../../components/CustomInput";
 import CustomButton from "../../components/CustomButton";
 
 export default function MotivoCancelacion() {
-  const dispatch = useDispatch();
   const navigation = useNavigation();
-  const [isLoading, setIsLoading] = useState(false);
   const { control, handleSubmit } = useForm();
-  const { user } = useSelector((state) => state.auth);
-  const { hcd, hcdConfig, arr_hcd } = useSelector((state) => state.hcd);
-
-  const onPressGuardar = async (data) => {
-    setIsLoading(true);
-    const finalHcd = {
-      ...hcd,
-      ...data,
-      key: uuidv5("", uuidv5.DNS),
-      id_interno: uuidv4(),
-      version_app: Application.nativeBuildVersion,
-    };
-    await dispatch(
-      addHcd({ new_arr_hcd: [...arr_hcd, finalHcd], hcdConfig, user })
-    ).unwrap();
-    dispatch(setHcdScreen(""));
-    navigation.navigate("HomeTab");
-    setIsLoading(false);
-  };
+  const { isLoading, finishHcd } = useFinishHcd();
 
   return (
     <Container>
@@ -49,7 +23,7 @@ export default function MotivoCancelacion() {
 
       <CustomButton
         text="CONFIRMAR"
-        onPress={handleSubmit(onPressGuardar)}
+        onPress={handleSubmit(finishHcd)}
         disabled={isLoading}
       />
       <CustomButton
