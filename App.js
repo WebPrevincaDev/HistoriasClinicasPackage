d80832cf5762f8b9403593b99ac8f46abca2c79e
Adrian Borella <adriannborella@gmail.com>
2025-04-25 10:52:51 -0300

    Configuracion de sentry

--- App.js
+++ App.js
@@ -11,12 +11,22 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
+//Sentry
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://a80eb9d4e68064aacd055e6aac14dc8b@o393307.ingest.us.sentry.io/4509214201544704",
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+});
+
 let persistor = persistStore(store);
 
 DropDownPicker.setListMode("MODAL");
 DropDownPicker.setLanguage("ES");
 
-export default function App() {
+function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -34,3 +44,5 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
   },
 });
+
+export default Sentry.wrap(App);
